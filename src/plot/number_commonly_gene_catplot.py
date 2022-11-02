b593f5c6ebf2f9a1841bdcb32a9b868436d62df2
edge2992 <torune.summer@gmail.com>
2022-11-02 23:14:50 +0900

    [plot] アッセイごとの遺伝子の被りを箱ひげ図で書く

---
+++ src/plot/number_commonly_gene_catplot.py
@@ -0,0 +1,75 @@
+# タンパク質の種類ごとに
+# 他のアッセイとの遺伝子の被りの個数を箱ひげ図で表示する
+
+# %%
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+from src.plot.util.process_intersect_gene import count_interection
+from src.plot.util.process_report import label_protein_biosample
+from src.util.bedfile import load_report
+
+load_dotenv()
+PROJECT_PATH = os.environ["PROJECT_PATH"]
+
+# %%
+
+
+def split_into_n(lst, n):
+    """Split the list into N pieces by splitting it from the front"""
+    return [list(tmp) for tmp in np.array_split(lst, n)]
+
+
+def split_dataframe(df: pd.DataFrame, n):
+    """Split the dataframe into N pieces by splitting it from the front"""
+    return [df.loc[tmp] for tmp in np.array_split(df.index, n)]
+
+
+# %%
+# 被りを数える
+
+report = load_report()
+
+count_all = count_interection(
+    report[report["Biological replicates"] == "1,2"], label_protein_biosample
+)
+
+
+# %%
+# 全てのタンパク質でそれぞれ箱ひげ図を書く
+SPLIT_NUM = 10
+
+fig, axes = plt.subplots(SPLIT_NUM, 1, figsize=((400 / SPLIT_NUM), 20 * SPLIT_NUM))
+dfs = split_dataframe(count_all, SPLIT_NUM)
+
+color_dict = {
+    target: sns.color_palette()[i]  # type: ignore
+    for i, target in enumerate(count_all.columns.map(lambda x: x.split()[1]).unique())
+}
+
+for ax, data in zip(axes, dfs):
+    palette = [color_dict[label.split()[1]] for label in data.index]
+    sns.boxplot(data=data.T, ax=ax, palette=palette)
+
+fig.savefig(
+    os.path.join(
+        PROJECT_PATH, "src", "plot", "img", "number_commonly_gene_violinplot.png"
+    )
+)
+
+# %%
+# 箱ひげ図を一つ書く
+target_protein = "BUD13"
+
+report_ex = report[
+    (report["Target label"] == target_protein)
+    & (report["Biological replicates"] == "1,2")
+]
+n_catplot = report_ex.shape[0]  # seriesでも大丈夫?
+data = count_all.loc[label_protein_biosample(report_ex)]
+sns.catplot(data=data.T, kind="violin")
+
+# %%
