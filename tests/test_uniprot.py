ac188d67073c27eca820435a7be4735f0792a2bc
edge2992 <torune.summer@gmail.com>
2022-11-10 17:11:37 +0900

    [refactor] uniprotのkeywordを読み込む関数を抽出

--- tests/test_uniprot.py
+++ tests/test_uniprot.py
@@ -6,3 +6,33 @@ def test_idmapping():
     d = idmapping()
     for protein in eclip:
         assert protein in d.keys()
+
+
+def test_keyword_count():
+    """keywordの出現順と個数をまとめたDataFrameを返す"""
+    from src.util.uniprot import keyword_count
+
+    df = keyword_count()
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
+
+
+def test_load_keyword_reprot():
+    """タンパク質ごとにキーワードをまとめたDataFrameを返す"""
+    from src.util.uniprot import load_keyword_report
+
+    df = load_keyword_report()
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
+
+
+def test_load_splited_keyword_report():
+    from src.util.uniprot import load_splited_keyword_report
+
+    seri = load_splited_keyword_report()
+    # assert seri.index.nunique() == 159
+    assert seri["From"].nunique() == 159
+    print(seri)
+    print(seri.shape)
