7c00d67ad3957ee5f9ba6e6378109b8725acd430
edge2992 <torune.summer@gmail.com>
2022-12-07 18:23:20 +0900

    HepG2の代表セットの確認

--- src/plot/interaction_metrics/del_small_interactions_set.py
+++ src/plot/interaction_metrics/del_small_interactions_set.py
@@ -3,44 +3,29 @@
 
 
 # %%
+import os
+import numpy as np
 import pandas as pd
+import seaborn as sns
+from dotenv import load_dotenv
+from scipy.spatial.distance import squareform
+
 from src.plot.util.process_report import count_gene
 from src.util.bedfile import load_replicateIDR_report
+from src.util.similarity_protein import InteractionSimilarity, ProteinSimilarity
+from src.util.similarity_strategy import TAPE, BlastP, KeywordCosine, Simpson
 
-from src.util.similarity_protein import ProteinSimilarity, InteractionSimilarity
-from src.util.similarity_strategy import BlastP, TAPE, KeywordCosine, Simpson
-
+load_dotenv()
+PROJECT_PATH = os.environ["PROJECT_PATH"]
 # %%
-# %%
 
-# %%
-similarity = ProteinSimilarity()
-
-similarity.setStrategy(BlastP())
-blastp = similarity.executeStrategy(transform=True)
-
-similarity.setStrategy(TAPE())
-tape = similarity.executeStrategy(transform=True)
-
-similarity.setStrategy(KeywordCosine())
-keyword = similarity.executeStrategy(transform=True)
-
-# %%
-similarity = InteractionSimilarity()
-similarity.setStrategy(Simpson())
-simpson = similarity.executeStrategy()
-
-# %%
-simpson.head()
-
-# %%
-assert all(tape.columns == simpson.columns)
-assert all(tape.columns == keyword.columns)
-assert all(tape.columns == blastp.columns)
-
-# %%
+SAVEDIR = os.path.join(PROJECT_PATH, "src/plot/img", "del_small_interactions_set")
 THRESHOLD_GENE_NUM = 1000
 BIOSAMPLE = "HepG2"
+TOPN = 200
+
+if not os.path.exists(SAVEDIR):
+    os.makedirs(SAVEDIR)
 
 
 def target_report(threshold_gene_num: int, biosample: str):
@@ -95,142 +80,58 @@ def metrics(report: pd.DataFrame):
 
 report = target_report(THRESHOLD_GENE_NUM, BIOSAMPLE)
 data = metrics(report)
-
 # %%
-data.head()
+tape_data = data.sort_values("TAPE").reset_index(drop=True)
+tape_data["label"] = [
+    "TOP{}".format(TOPN) if i else "others" for i in data.index < TOPN
+]
 
-# %%
-data.sort_values("TAPE").head(10)
-
-# %%
-index_n = np.where(np.triu(squareform(np.ones(1081))))
-desc1 = (
-    report.iloc[index_n[0]]
-    .loc[:, ["Dataset", "Target label", "Biosample name"]]
-    .reset_index(drop=True)
-    .add_suffix("_1")
+splot = sns.pairplot(
+    tape_data,
+    hue="label",
+    vars=["TAPE", "keyword", "blastp", "simpson"],
+    corner=True,
+    palette={
+        "TOP{}".format(TOPN): sns.color_palette()[1],  # type: ignore
+        "others": sns.color_palette()[0],  # type: ignore
+    },
+    plot_kws={"alpha": 0.3},
 )
-desc2 = (
-    report.iloc[index_n[1]]
-    .loc[:, ["Dataset", "Target label", "Biosample name"]]
-    .reset_index(drop=True)
-    .add_suffix("_2")
-)
-
-# %%
-# print(desc2)
-# print(desc1)
-dd = pd.concat([desc1, desc2], axis=1)
-pd.concat([dd, data], axis=1)
-# %%
-report["Dataset"]
-
-
-# %%
-print(data.head())
-data.shape
-
-
-# %%
-data = pd.DataFrame(
-    {
-        "TAPE": similarity.flatten_tri(
-            tape.loc[target_accessions, target_accessions], False
-        ),
-        "Keyword": similarity.flatten_tri(
-            keyword.loc[target_accessions, target_accessions], False
-        ),
-        "blastp": similarity.flatten_tri(
-            blastp.loc[target_accessions, target_accessions], False
-        ),
-        "simpson": similarity.flatten_tri(
-            simpson.loc[target_accessions, target_accessions], False
-        ),
-    }
+splot.fig.suptitle("pairplot of TAPE, keyword, blastp, simpson sorted by TAPE")
+splot.fig.subplots_adjust(top=0.95)
+splot.fig.savefig(
+    os.path.join(SAVEDIR, "pairplot_sorted_by_TAPE.png"), dpi=300, bbox_inches="tight"
 )
 
 # %%
-from scipy.spatial.distance import squareform
-import numpy as np
 
-TOPN = 1000
-data["rank"] = data["TAPE"].rank()
-mat = np.triu(squareform(data["rank"] <= TOPN))
-index_topN = np.where(mat == True)
+simpson_data = data.sort_values("simpson", ascending=False).reset_index(drop=True)
+simpson_data["label"] = [
+    "TOP{}".format(TOPN) if i else "others" for i in data.index < TOPN
+]
 
-# %%
-protein_data = pd.DataFrame(
-    {
-        "dataset1": [target_accessions[i] for i in index_topN[0]],
-        "dataset2": [target_accessions[i] for i in index_topN[1]],
-        "TAPE": squareform(data["TAPE"])[index_topN],
-        "blastp": squareform(data["blastp"])[index_topN],
-        "keyword": squareform(data["Keyword"])[index_topN],
-        "simpson": squareform(data["simpson"])[index_topN],
-    }
+splot = sns.pairplot(
+    simpson_data,
+    hue="label",
+    vars=["TAPE", "keyword", "blastp", "simpson"],
+    corner=True,
+    palette={
+        "TOP{}".format(TOPN): sns.color_palette()[1],  # type: ignore
+        "others": sns.color_palette()[0],  # type: ignore
+    },
+    plot_kws={"alpha": 0.3},
 )
-protein_data.head()
-# %%
-# tapeの順位100位を取って
-# dataset1, protien1, cell1, dataset2, protein2, cell2, tape, blastp, keyword, simpsonのデータフレームを作成する
-
-# %%
-report = load_replicateIDR_report()[["Dataset", "Target label", "Biosample name"]]
-report.head()
-
-# %%
-targets = (
-    pd.merge(
-        pd.merge(
-            protein_data, report, left_on="dataset1", right_on="Dataset", how="inner"
-        ),
-        report,
-        left_on="dataset2",
-        right_on="Dataset",
-        how="inner",
-        suffixes=("_1", "_2"),
-    )
-    .drop(columns=["dataset1", "dataset2"])
-    .sort_values("TAPE", ascending=True)
+splot.fig.suptitle("pairplot of TAPE, keyword, blastp, simpson sorted by TAPE")
+splot.fig.subplots_adjust(top=0.95)
+splot.fig.savefig(
+    os.path.join(SAVEDIR, "pairplot_sorted_by_simpson.png"),
+    dpi=300,
+    bbox_inches="tight",
 )
 
 # %%
-targets[
-    ["Target label_1", "Target label_2", "Biosample name_1", "Biosample name_2"]
-].head()
-
-# %%
-hepg2_targets = (
-    targets[
-        (targets["Biosample name_1"] == "HepG2")
-        & (targets["Biosample name_2"] == "HepG2")
-    ]
-    .reset_index(drop=True)
-    .head(200)
-)
-
-# %%
-hepg2_targets[["Target label_1", "Target label_2", "blastp", "simpson"]].to_csv(
-    "hepg2_targets.csv", index=False
-)
-
-# %%
-data["blastp"].describe()
-
-# %%
-hepg2_targets["blastp"].describe()
+simpson_data.head()
 
 # %%
 
-data["TAPE"].describe()
-
-
-# %%
-hepg2_targets["TAPE"].describe()
-# %%
-data["simpson"].describe()
-
-# %%
-hepg2_targets["simpson"].describe()
-
-# %%
+# simpsonスコアが高い実験ペアの遺伝子のセットを取得して、比較してみる
