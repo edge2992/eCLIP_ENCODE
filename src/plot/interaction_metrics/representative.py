17b10bb455c072c9bbd74c96b10f7eb0270295f2
edge2992 <torune.summer@gmail.com>
2022-12-15 16:57:17 +0900

    type ignore, delete method (not used)

--- src/plot/interaction_metrics/representative.py
+++ src/plot/interaction_metrics/representative.py
@@ -1,26 +1,12 @@
 # このフォルダで使用する便利関数
-from typing import Callable, Dict
-import numpy as np
 import pandas as pd
-from scipy.spatial.distance import squareform
 
-from src.plot.util.process_report import count_gene
-from src.util.bedfile import load_replicateIDR_report, read_annotated_bed
-from src.util.similarity_protein import InteractionSimilarity, ProteinSimilarity
-from src.util.similarity_strategy import (
-    SimilarityStrategy,
-    TAPE,
-    BlastP,
-    Cosine,
-    KeywordCosine,
-    Lift,
-    Simpson,
-)
-from src.util.get_bed_path import get_formatted_file_path
-from src.util.bed_format_strategy import FormatStrategy
-from src.util.uniprot import load_keyword_report
 from src.eclip import Compare, Dataset
-from src.util.metrics.metrics import Metrics
+from src.plot.util.process_report import count_gene
+from src.util.bed_format_strategy import FormatStrategy
+from src.util.bedfile import load_replicateIDR_report, read_annotated_bed
+from src.util.get_bed_path import get_formatted_file_path
+from src.util.uniprot import load_keyword_report
 
 
 def target_report(threshold_gene_num: int, biosample: str):
@@ -34,79 +20,6 @@ def target_report(threshold_gene_num: int, biosample: str):
         (count_gene(report, lambda row: row["Dataset"]) >= threshold_gene_num).to_list()
     ]
     return target_report
-
-
-def similarity_strategy_dict():
-    protein_strategies = {
-        "TAPE": lambda report: TAPE(report),
-        "keyword": lambda report: KeywordCosine(report),
-        "blastp": lambda report: BlastP(
-            report, symmetric=True, symmetric_method="average"
-        ),
-    }
-    interaction_strategies = {
-        "simpson": lambda report: Simpson(report),
-        "lift": lambda report: Lift(report),
-        "cosine": lambda report: Cosine(report),
-    }
-    return protein_strategies, interaction_strategies
-
-
-def metrics(
-    report: pd.DataFrame,
-    protein_strategies: Dict[
-        str,
-        Callable[[pd.DataFrame], SimilarityStrategy],
-    ],
-    interaction_strategies: Dict[
-        str,
-        Callable[[pd.DataFrame], SimilarityStrategy],
-    ],
-    sort_by: str = "TAPE",
-):
-    """統計値を整形する
-    タンパク質の類似度: TAPE(cosine distance), blastp(bit score), keyword(cosine distance)
-    相互作用の遺伝子の類似度: simpson index
-    """
-    p_similairty = ProteinSimilarity()
-    inter_similarity = InteractionSimilarity()
-
-    def protein_similarity(strategy):
-        p_similairty.setStrategy(strategy)
-        return p_similairty.flatten_tri(
-            p_similairty.executeStrategy(transform=True), False
-        )
-
-    def interaction_similarity(strategy):
-        inter_similarity.setStrategy(strategy)
-        return inter_similarity.flatten_tri(inter_similarity.executeStrategy(), False)
-
-    REPORT_COLUMNS = ["Dataset", "Target label", "Biosample name"]
-
-    dict_data = {}
-    for label, strategy in protein_strategies.items():
-        dict_data[label] = protein_similarity(strategy(report))
-
-    for label, strategy in interaction_strategies.items():
-        dict_data[label] = interaction_similarity(strategy(report))
-
-    data = pd.DataFrame(dict_data)
-    index_n = np.where(np.triu(squareform(np.ones(data.shape[0]))))
-
-    desc = pd.concat(
-        [
-            report.iloc[index_n[0]]
-            .loc[:, REPORT_COLUMNS]
-            .reset_index(drop=True)
-            .add_suffix("_1"),
-            report.iloc[index_n[1]]
-            .loc[:, REPORT_COLUMNS]
-            .reset_index(drop=True)
-            .add_suffix("_2"),
-        ],
-        axis=1,
-    )
-    return pd.concat([desc, data], axis=1).sort_values(sort_by).reset_index(drop=True)
 
 
 def get_geneset(dataset: str, how=FormatStrategy.MAX):
