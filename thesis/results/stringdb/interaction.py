9e0acd2b7082967606fd5cb1206ffe18232820cb
edge2992 <torune.summer@gmail.com>
2023-01-04 22:50:11 +0900

    plot: 統計情報　STRINGDB, INTERACTION

--- thesis/results/stringdb/interaction.py
+++ thesis/results/stringdb/interaction.py
@@ -5,24 +5,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import seaborn as sns
-from src.eclip.sampleset import SampleSetECLIP
 from src.util.metrics import Metrics
-from src.util.metrics.condition import ConditionEq
 from src.util.similarity_strategy import (
     INTERACTION_SIMILARITY_STRATEGIES,
     DirectStringScore,
 )
+from thesis.utils.reportset import COMPARE_REPORT_SET
 from thesis.utils.matplotlib_format import MATPLOTLIB_CONFIG
+from thesis.utils.tex_escape import tex_escape
 
 load_dotenv()
 
 THESIS_FIG_PATH = os.environ["THESIS_FIG_PATH"]
 THESIS_TB_PATH = os.environ["THESIS_TB_PATH"]
-SAVEDIR = os.path.join(THESIS_FIG_PATH, "results")
-TB_SAVEDIR = os.path.join(THESIS_TB_PATH, "results")
-
-if not os.path.exists(SAVEDIR):
-    os.makedirs(SAVEDIR)
+TB_SAVEDIR = os.path.join(THESIS_TB_PATH, "results", "stringdb", "interaction")
 
 if not os.path.exists(TB_SAVEDIR):
     os.makedirs(TB_SAVEDIR)
@@ -32,72 +28,76 @@ for key, value in MATPLOTLIB_CONFIG.items():
 
 # %%
 
+for biosample in COMPARE_REPORT_SET:
+    for condition in COMPARE_REPORT_SET[biosample]:
+        sampleset = COMPARE_REPORT_SET[biosample][condition]
 
-interaction_data = {
-    k: Metrics(SampleSetECLIP(ConditionEq("Biosample name", k)).report)(
-        INTERACTION_SIMILARITY_STRATEGIES, add_description=False
-    )
-    for k in ["HepG2", "K562"]
-}
-
-# %%
-
-string_data = {
-    k: Metrics(SampleSetECLIP(ConditionEq("Biosample name", k)).report)(
-        DirectStringScore(metrics="score"), add_description=False
-    )
-    for k in ["HepG2", "K562"]
-}
-
-# %%
-idx = pd.IndexSlice
-for biosample in ["HepG2", "K562"]:
-    desc = (
-        pd.concat(
+        data: pd.DataFrame = Metrics(sampleset.report)(
             [
-                interaction_data[biosample].rename(
-                    {"Peak N_intersections": "Peak N\_intersections"}, axis=1
-                ),
-                pd.cut(
-                    string_data[biosample],  # type: ignore
-                    bins=[-0.1, 0.4, 0.6, 1],
-                    labels=["low-confidence", "midium-confidence", "high-confidence"],
-                ),
+                *INTERACTION_SIMILARITY_STRATEGIES,
+                DirectStringScore(metrics="score", qcut=True),
             ],
-            axis=1,
+            add_description=False,
+        ).replace(
+            {
+                "STRING Score": {
+                    "h": "high-confidence",
+                    "m": "midium-confidence",
+                    "l": "low-confidence",
+                }
+            },
+        )  # type: ignore
+
+        metrics_table = data.groupby("STRING Score").agg(
+            {"Gene Jaccard": ["count", "mean", "std"], "Gene Simpson": ["mean", "std"]}
         )
-        .groupby("STRING Score")
-        .describe()
-        .T
-    )
-    desc.loc[idx[:, ["count", "mean", "std"]], :].style.format(
-        {"mean": "{:.2f}", "std": "{:.2f}", "count": "{:,.0f}"}
-    ).to_latex(
-        os.path.join(
-            TB_SAVEDIR, f"interaction_metrics_grouped_stringdb_{biosample}.tex"
-        ),
-        position="htbp",
-        position_float="centering",
-        hrules=True,
-        caption=f"相互作用類似度指標の代表値 ({biosample}",
-        label=f"tab:interaction_metrics_grouped_stringdb_{biosample}",
-    )
+
+        metrics_table.style.format(
+            {
+                ("Gene Jaccard", "mean"): "{:.2e}",
+                ("Gene Jaccard", "std"): "{:.2e}",
+                ("Gene Simpson", "mean"): "{:.2e}",
+                ("Gene Simpson", "std"): "{:.2e}",
+                ("Gene Jaccard", "count"): "{:,d}",
+            }
+        ).format_index(escape="latex", axis=1).format_index(
+            escape="latex", axis=0
+        ).to_latex(
+            os.path.join(
+                TB_SAVEDIR,
+                f"metrics_table_string_interaction_{biosample}_{condition}.tex",
+            ),
+            position="htbp",
+            position_float="centering",
+            hrules=True,
+            caption=tex_escape(
+                f"STRING Scoreで分けた時の相互作用類似指標の代表値 ({condition} ({biosample}))"
+            ),
+            label="tab:metrics_string_interaction_{biosample}_{condition}",
+        )
 
 
 # %%
 # heavy no meaning
+condition = "Protein_GTE_1000"
+
 for biosample in ["HepG2", "K562"]:
-    data = pd.concat(
+    sampleset = COMPARE_REPORT_SET[biosample][condition]
+    data: pd.DataFrame = Metrics(sampleset.report)(
         [
-            interaction_data[biosample],
-            pd.cut(
-                string_data[biosample],  # type: ignore
-                bins=[-0.1, 0.4, 0.6, 1],
-                labels=["low-confidence", "midium-confidence", "high-confidence"],
-            ),
+            *INTERACTION_SIMILARITY_STRATEGIES,
+            DirectStringScore(metrics="score", qcut=True),
         ],
-        axis=1,
-    )
+        add_description=False,
+    ).replace(
+        {
+            "STRING Score": {
+                "h": "high-confidence",
+                "m": "midium-confidence",
+                "l": "low-confidence",
+            }
+        },
+    )  # type: ignore
 
     splot = sns.pairplot(
         data,
@@ -110,9 +110,9 @@ for biosample in ["HepG2", "K562"]:
         plot_kws=dict(alpha=0.15, edgecolor="none", s=70),
     )
 
-    splot.fig.savefig(
-        os.path.join(SAVEDIR, f"interaction_stringdb_metrics_pairplot_{biosample}.pdf"),
-        dpi=300,
-    )
+    # splot.fig.savefig(
+    #     os.path.join(SAVEDIR, f"interaction_stringdb_metrics_pairplot_{biosample}.pdf"),
+    #     dpi=300,
+    # )
 
 # %%
