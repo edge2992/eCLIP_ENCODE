aada97cef779959d520b0326ff3f8e6cd27c8e9c
edge2992 <torune.summer@gmail.com>
2023-01-05 10:14:09 +0900

    checked gene type

--- thesis/results/propotion/map_gene.py
+++ thesis/results/propotion/map_gene.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 from src.eclip.sampleset import SampleSetECLIP
-from src.util.metrics.condition import ConditionEq
+from src.util.metrics.condition import ConditionEq, ConditionAnd
 
 load_dotenv()
 
@@ -53,4 +53,34 @@ for biosample in ["HepG2", "K562"]:
         caption=f"bedtools intersectによって変換された後の遺伝子の種類の割合 ({biosample})",
         label=f"tab:gene_type_{biosample}",
     )
+# %%
+
+# DKC1 HepG2, TROVE2 K562
+targets = {"HepG2": "DKC1", "K562": "TROVE2"}
+for biosample, target in targets.items():
+    report = SampleSetECLIP(
+        ConditionAnd(
+            [
+                ConditionEq("Biosample name", biosample),
+                ConditionEq("Target label", target),
+            ]
+        )
+    ).report
+    data = squash_columns(count_gene_type(report), 0).iloc[0]
+    ratio_table = pd.DataFrame(
+        {"count": data, "proportion": data / data.sum() * 100}
+    ).sort_values("count", ascending=False)
+    print(biosample, target)
+    print(ratio_table.head())
+
+# %%
+# 90%以上がprotein_codingのものを数える
+
+for biosample in ["HepG2", "K562"]:
+    report = SampleSetECLIP(ConditionEq("Biosample name", biosample)).report
+    data = squash_columns(count_gene_type(report), 0)
+    total_value = data.sum(axis=1).to_numpy().reshape(-1, 1)
+    data = data.div(total_value, axis=1)
+    result = (data["protein_coding"] >= 0.9).sum()
+    print(data.shape, result)
 # %%
